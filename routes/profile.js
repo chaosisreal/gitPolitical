5cbd124dbd69f43ee8780b00559ca7adca76e92d
chaosisreal <eric2010nut@gmail.com>
2017-05-06 05:23:19 -0500

    Add Topic database

--- routes/profile.js
+++ routes/profile.js
@@ -1,25 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-// Post
-router.get('/get-data', function(req, res, next){
-	postData.find()
-		.then(function(doc) {
-			res.render('/', {items: doc});
-		});
-});
-
-router.post('/insert', function(req, res, next){
-		var item = {
-			title: req.body.tile,
-			message: req.body.message
-		};
-
-		var data = new postData(item);
-		data.save();
-
-		res.redirect('/');
-});
 
 // Get Homepage
 router.get('/dashboard', ensureAuthenticated, function(req, res){
@@ -31,44 +12,29 @@ function ensureAuthenticated(req, res, next){
 		return next();
 	} else {
 		req.flash('error_msg','You are not logged in');
-		res.redirect('/');
+		res.render('login');
 	}
 }
 
-// 	var title = req.body.name;
-// 	var message = req.body.email;
-//
-// 	// Validation
-// 	req.checkBody('title', 'Title is required').notEmpty();
-// 	req.checkBody('message', 'Message is required').notEmpty();
-//
-// 	var errors = req.validationErrors();
-//
-// 	if(errors){
-// 		res.render('profile',{
-// 			errors:errors
+module.exports = router;
+
+// // Post
+// router.get('/get-data', function(req, res, next){
+// 	postData.find()
+// 		.then(function(doc) {
+// 			res.render('/', {items: doc});
 // 		});
-// 	} else {
-// 		var newPost = new Forum({
-// 			title: title,
-// 			message: message,
-// 		});
+// });
 //
-// 		Forum.save(newPost, function(err){
-// 			if(err){
-// 				console.log(err);
-// 			} else {
-// 				console.log('It Worked.');
-// 			}
+// router.post('/insert', function(req, res, next){
+// 		var item = {
+// 			title: req.body.tile,
+// 			message: req.body.message
+// 		};
 //
-//
-// 		});
-//
-// 		req.flash('success_msg', 'Your topic is on the front page!');
+// 		var data = new postData(item);
+// 		data.save();
 //
 // 		res.redirect('/');
-// 	}
 // });
-
-
-module.exports = router;
+//
