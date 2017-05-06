6a20749e9b8228e354dba21d0dd78e60aebe4087
chaosisreal <eric2010nut@gmail.com>
2017-05-05 23:34:44 -0500

    Topic removal

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var Topic = require('../models/topic');
-
 // Get Homepage
 router.get('/dashboard', ensureAuthenticated, function(req, res){
 	res.render('profile');
@@ -17,36 +15,37 @@ function ensureAuthenticated(req, res, next){
 	}
 }
 
-// Topic
-router.post('/topic', function(req, res){
-	var topic = req.body.name;
-	var message = req.body.email;
-
-	// Validation
-	req.checkBody('topic', 'Topic is required').notEmpty();
-	req.checkBody('message', 'Message is required').notEmpty();
-
-	var errors = req.validationErrors();
-
-	if(errors){
-		res.get('/dashboard',{
-			errors:errors
-		});
-	} else {
-		var newTopic = new Topic({
-			topic: topic,
-			message: message,
-		});
-
-		User.createUser(newTopic, function(err, topic){
-			if(err) throw err;
-			console.log(topic);
-		});
-
-		req.flash('success_msg', 'Your topic is on the front page!');
-
-	}
-});
+//
+// // Topic
+// router.post('/topic', function(req, res){
+// 	var topic = req.body.name;
+// 	var message = req.body.email;
+//
+// 	// Validation
+// 	req.checkBody('topic', 'Topic is required').notEmpty();
+// 	req.checkBody('message', 'Message is required').notEmpty();
+//
+// 	var errors = req.validationErrors();
+//
+// 	if(errors){
+// 		res.get('/dashboard',{
+// 			errors:errors
+// 		});
+// 	} else {
+// 		var newTopic = new Topic({
+// 			topic: topic,
+// 			message: message,
+// 		});
+//
+// 		User.createUser(newTopic, function(err, topic){
+// 			if(err) throw err;
+// 			console.log(topic);
+// 		});
+//
+// 		req.flash('success_msg', 'Your topic is on the front page!');
+//
+// 	}
+// });
 
 
 module.exports = router;
