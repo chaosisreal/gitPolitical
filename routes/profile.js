c86597cba830a9d55587b8be2a3c0461a0b05ed9
chaosisreal <eric2010nut@gmail.com>
2017-05-06 03:03:49 -0500

    Add Topic database

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var Topic = require('../models/topic');
+
 // Get Homepage
 router.get('/dashboard', ensureAuthenticated, function(req, res){
 	res.render('profile');
@@ -15,37 +17,38 @@ function ensureAuthenticated(req, res, next){
 	}
 }
 
-//
-// // Topic
-// router.post('/topic', function(req, res){
-// 	var topic = req.body.name;
-// 	var message = req.body.email;
-//
-// 	// Validation
-// 	req.checkBody('topic', 'Topic is required').notEmpty();
-// 	req.checkBody('message', 'Message is required').notEmpty();
-//
-// 	var errors = req.validationErrors();
-//
-// 	if(errors){
-// 		res.get('/dashboard',{
-// 			errors:errors
-// 		});
-// 	} else {
-// 		var newTopic = new Topic({
-// 			topic: topic,
-// 			message: message,
-// 		});
-//
-// 		User.createUser(newTopic, function(err, topic){
-// 			if(err) throw err;
-// 			console.log(topic);
-// 		});
-//
-// 		req.flash('success_msg', 'Your topic is on the front page!');
-//
-// 	}
-// });
+
+// Topic
+router.get('/topic', function(req, res){
+	var topic = req.body.name;
+	var message = req.body.email;
+
+	// Validation
+	req.checkBody('topic', 'Topic is required').notEmpty();
+	req.checkBody('message', 'Message is required').notEmpty();
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('profile',{
+			errors:errors
+		});
+	} else {
+		var newTopic = new Topic({
+			topic: topic,
+			message: message,
+		});
+
+		Topic.createTopic(newTopic, function(err, topic){
+			if(err) throw err;
+			console.log(topic);
+		});
+
+		req.flash('success_msg', 'Your topic is on the front page!');
+
+		res.redirect('/');
+	}
+});
 
 
 module.exports = router;
