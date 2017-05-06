2a2a30250f108a2157bd5849a27ede8caa8813c6
chaosisreal <eric2010nut@gmail.com>
2017-05-05 21:34:52 -0500

    updates

--- routes/profile.js
+++ routes/profile.js
@@ -11,8 +11,40 @@ function ensureAuthenticated(req, res, next){
 		return next();
 	} else {
 		//req.flash('error_msg','You are not logged in');
-		res.redirect('/users/login');
+		res.redirect('/');
 	}
 }
+
+// Topic
+router.post('/topic', function(req, res){
+	var topic = req.body.name;
+	var message = req.body.email;
+
+	// Validation
+	req.checkBody('topic', 'Topic is required').notEmpty();
+	req.checkBody('message', 'Message is required').notEmpty();
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.get('/dashboard',{
+			errors:errors
+		});
+	} else {
+		var newTopic = new Topic({
+			topic: topic,
+			message: message,
+		});
+
+		User.createUser(newTopic, function(err, topic){
+			if(err) throw err;
+			console.log(topic);
+		});
+
+		req.flash('success_msg', 'Your topic is on the front page!');
+
+	}
+});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -50,8 +50,7 @@ router.post('/register', function(req, res){
 			console.log(user);
 		});
 
-		req.flash('success_msg', 'You are registered and can now login');
-
+		res.redirect('/');
 	}
 });
 
